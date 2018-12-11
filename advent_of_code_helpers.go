d2c72749f97a232106dc60277d62f8e8f0fb0d0b
Paulius Stundzia <paulius.stundzia@tesonet.com>
2018-12-11 09:10:13 +0200

    MOAR HELPERS!

--- advent_of_code_helpers.go
+++ advent_of_code_helpers.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"path/filepath"
+	"strings"
 )
 
 func failOnError(err error, msg string) {
@@ -22,4 +23,69 @@ func LoadInputTxt(day int) string {
 		failOnError(err, "Could not load file")
 	}
 	return string(dat)
+}
+
+func LoadInputAsStringSlice(day int) []string {
+	input := LoadInputTxt(day)
+	return strings.Split(input, "\n")
+}
+
+func SortRuneSlice(runeSlice []rune) []rune {
+	res := []rune{}
+	for len(runeSlice) > 0 {
+		var lastItem rune = -1
+		var lastItemIndex int = -1
+		for i, item := range runeSlice {
+			if lastItem == -1 {
+				lastItem = item
+				lastItemIndex = i
+				continue
+			}
+			if item <= lastItem {
+				lastItem = item
+				lastItemIndex = i
+			}
+		}
+		res = append(res, lastItem)
+		if len(runeSlice) == 1 {
+			runeSlice = []rune{}
+		} else {
+			runeSlice = append(runeSlice[:lastItemIndex], runeSlice[lastItemIndex+1:]...)
+		}
+	}
+	return res
+}
+
+func DeleteItemFromRuneSlice(slice []rune, itemToPop rune)  (resSlice []rune, foundAndDeleted bool) {
+	for index, item := range slice {
+		if item == itemToPop {
+			slice = append(slice[:index], slice[index+1:]...)
+			return slice, true
+		}
+	}
+	return slice, false
+}
+
+func InsertIntoIntSliceCircular(slice []int, value int, index int) (resSlice []int, actualIndex int) {
+	for index < 0 {
+		index = len(slice) + index + 1
+	}
+	if index >= len(slice) {
+		index = index % len(slice)
+	}
+	slice = append(slice, 0)
+	copy(slice[index + 1:], slice[index:])
+	slice[index] = value
+	return slice, index
+}
+
+func PopFromIntSlice(slice []int, index int) (resSlice []int, res int, actualIndex int) {
+	for index < 0 {
+		index = len(slice) + index + 1
+	}
+	val := slice[index]
+	copy(slice[index:], slice[index + 1:])
+	slice[len(slice) - 1] = 0
+	slice = slice[:len(slice) - 1]
+	return slice, val, index
 }
